6c65e0ceb482ee5ca8925f86e24f45260f5af723
Alena <morven2018@yandex.ru>
2025-07-06 00:08:24 +0300

    chore: add checks to eslint.config

--- rs-react-app/eslint.config.js
+++ rs-react-app/eslint.config.js
@@ -1,23 +1,43 @@
-import js from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
+      eslintPluginPrettier,
     ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-]);
+    plugins: {
+      react,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-console': 'warn',
+      'react-compiler/react-compiler': 'error',
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }
+);
