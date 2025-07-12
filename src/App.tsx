b5c2f3fce3522008d5886d0910cfaf6aa416f6cb
Alena <morven2018@yandex.ru>
2025-07-12 23:10:47 +0300

    refactor: rewrite to class component initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import { useState } from 'react';
+import { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  state = {
+    count: 0,
+  };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+  handleIncrement = () => {
+    this.setState((prevState: { count: number }) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <a href="https://vite.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={this.handleIncrement}>
+            count is {this.state.count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
