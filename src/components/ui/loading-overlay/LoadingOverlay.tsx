e3cdc9c103bf9ea100be08a81c2caeb6c6ec9e83
Alena <morven2018@yandex.ru>
2025-07-13 11:34:57 +0300

    refactor: move loading to ui

--- src/components/ui/loading-overlay/LoadingOverlay.tsx
+++ src/components/ui/loading-overlay/LoadingOverlay.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spinner from '../spinner/Spinner';
+import Spinner from './spinner/Spinner';
 import styles from './LoadingOverlay.module.scss';
 
 const message = 'Loading...';
