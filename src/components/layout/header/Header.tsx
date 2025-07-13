7b0f097b464de4e27d7720a8be6af982f487f1b6
Alena <morven2018@yandex.ru>
2025-07-13 18:00:52 +0300

    feat: style header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '@assets/images/image.png';
+import style from './Header.module.scss';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <img src={logo} alt="Star Wars" />
+      <header className={style.header}>
+        <img src={logo} alt="White tree" className={style.whiteTree} />
         <div>
-          <h2>Middle Earth</h2>
-          <h3>Search.</h3>
+          <h1 className={style.h1}>Middle Earth</h1>
+          <h2 className={style.subheader}>Search</h2>
         </div>
       </header>
     );
