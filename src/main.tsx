8afb7ee65c3b6c0432fe9e907af4e5fdffbcd060
Alena <morven2018@yandex.ru>
2025-07-13 19:00:25 +0300

    fix: remove file of initial styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import '@assets/styles/app.scss';
-import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from '@components/common/ErrorBoundary.tsx';
 import { StrictMode } from 'react';
