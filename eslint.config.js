7d916a6dc24f0e7c9cb58c93d525aefc95a141d4
Alena <morven2018@yandex.ru>
2025-07-24 01:24:27 +0300

    chore: add hook in config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import js from '@eslint/js';
 import react from 'eslint-plugin-react';
 import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
@@ -22,17 +23,22 @@ export default tseslint.config(
     plugins: {
       react,
       'react-refresh': reactRefresh,
+      'react-hooks': reactHooks,
       'react-compiler': reactCompiler,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'no-console': ['warn', { allow: ['warn', 'error'] }],
       'react-compiler/react-compiler': 'error',
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      'react/prop-types': 'off',
+      'react/require-default-props': 'off',
     },
     settings: {
       react: {
