963ff214891f05e755052df4b0fe5dee06c5e946
Alena <morven2018@yandex.ru>
2025-07-23 23:06:04 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import './App.css';
+import AboutPage from './pages/about/about';
 import Header from '@components/layout/header/Header';
 import Main from '@components/layout/main/Main';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from './pages/not-found/not-found';
+
+const HOME_PATH = '/';
+const RESERVE_HOME_PATH = '/index.html';
+const ABOUT_PATH = '/about';
+const OTHER_PATH = '*';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <Main />
+        <Routes>
+          <Route path={HOME_PATH} element={<Main />} />
+          <Route path={RESERVE_HOME_PATH} element={<Main />} />
+          <Route path={ABOUT_PATH} element={<AboutPage />} />
+          <Route path={OTHER_PATH} element={<NotFoundPage />} />
+        </Routes>
       </div>
     );
   }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import App from './App.tsx';
 import ErrorBoundary from '@components/common/ErrorBoundary.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
 );
