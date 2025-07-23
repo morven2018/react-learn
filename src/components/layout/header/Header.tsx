92319cd31aa16ab6013ad9b6a8eed8b8faf704b8
Alena <morven2018@yandex.ru>
2025-07-23 22:37:19 +0300

    refactor: move header to functional style

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
 import logo from '@assets/images/image.png';
 import style from './Header.module.scss';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className={style.header}>
-        <img src={logo} alt="White tree" className={style.whiteTree} />
-        <div>
-          <h1 className={style.h1}>Middle Earth</h1>
-          <h2 className={style.subheader}>Search</h2>
-        </div>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <img src={logo} alt="White tree" className={style.whiteTree} />
+      <div>
+        <h1 className={style.h1}>Middle Earth</h1>
+        <h2 className={style.subheader}>Search</h2>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
