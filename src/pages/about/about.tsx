35a3c13e0663a11cd6faa4a82d361d2db06cc395
Alena <morven2018@yandex.ru>
2025-07-23 23:05:26 +0300

    feat: add 404 and about pages (blank)

---
+++ src/pages/about/about.tsx
@@ -0,0 +1,12 @@
+export const AboutPage = () => {
+  return (
+    <main>
+      <div>
+        <h2>about.</h2>
+        <button onClick={() => {}}>home</button>
+      </div>
+    </main>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,12 @@
+export const NotFoundPage = () => {
+  return (
+    <main>
+      <div>
+        <h2>This page doesn’t exist.</h2>
+        <button onClick={() => {}}>home</button>
+      </div>
+    </main>
+  );
+};
+
+export default NotFoundPage;
