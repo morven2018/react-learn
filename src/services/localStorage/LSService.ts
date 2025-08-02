669701d972502d71734021e6c8cfba3865e5fa06
Alena <morven2018@yandex.ru>
2025-08-02 06:12:54 +0300

    feat: add store redux state in LS

--- src/services/localStorage/LSService.ts
+++ src/services/localStorage/LSService.ts
@@ -1,24 +1,37 @@
-enum term {
+enum StoredTerm {
   LastSearch = 'lastSearchTerm',
   Theme = 'userTheme',
+  SelectedCharacters = 'selectedCharacters',
 }
 
 export type Themes = 'dark' | 'light';
 
 export class Term {
   public static getTermFromLS(): string | undefined {
-    return localStorage.getItem(term.LastSearch) ?? undefined;
+    return localStorage.getItem(StoredTerm.LastSearch) ?? undefined;
   }
 
   public static setTermToLS(newTerm: string): void {
-    localStorage.setItem(term.LastSearch, newTerm);
+    localStorage.setItem(StoredTerm.LastSearch, newTerm);
   }
 
   public static getThemeFromLS(): Themes {
-    return localStorage.getItem(term.Theme) ? 'light' : 'dark';
+    return localStorage.getItem(StoredTerm.Theme) ? 'light' : 'dark';
   }
 
   public static setThemeToLS(theme: Themes = 'dark'): void {
-    localStorage.setItem(term.Theme, theme);
+    localStorage.setItem(StoredTerm.Theme, theme);
+  }
+
+  public static setSelectedToLS(selected: string[]): void {
+    localStorage.setItem(
+      StoredTerm.SelectedCharacters,
+      JSON.stringify(selected)
+    );
+  }
+
+  public static getSelectedFromLS(): string[] {
+    const saved = localStorage.getItem(StoredTerm.SelectedCharacters);
+    return saved ? JSON.parse(saved) : [];
   }
 }
