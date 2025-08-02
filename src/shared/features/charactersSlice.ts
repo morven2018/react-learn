ed4954aed62f3b9c4cce96310da77d5bda18b958
Alena <morven2018@yandex.ru>
2025-08-02 06:01:34 +0300

    feat: add checkout state slice

---
+++ src/shared/features/charactersSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { Term } from '@services/localStorage/LSService';
+
+interface CharactersState {
+  selectedCharacters: string[];
+}
+
+const initialState: CharactersState = {
+  selectedCharacters: Term.getSelectedFromLS() ?? [],
+};
+
+export const charactersSlice = createSlice({
+  name: 'characters',
+  initialState,
+  reducers: {
+    toggleCharacterSelection: (state, action: PayloadAction<string>) => {
+      const index = state.selectedCharacters.indexOf(action.payload);
+      if (index === -1) {
+        state.selectedCharacters.push(action.payload);
+      } else {
+        state.selectedCharacters.splice(index, 1);
+      }
+      Term.setSelectedToLS(state.selectedCharacters);
+    },
+    clearSelectedCharacters: (state) => {
+      state.selectedCharacters = [];
+      Term.setSelectedToLS([]);
+    },
+  },
+});
+
+export const { toggleCharacterSelection, clearSelectedCharacters } =
+  charactersSlice.actions;
+export default charactersSlice.reducer;
