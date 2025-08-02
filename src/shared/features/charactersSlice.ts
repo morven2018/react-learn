b7379f626031a0733f8d9401cba3074ae3ab9107
Alena <morven2018@yandex.ru>
2025-08-02 21:16:07 +0300

    feat: add test to slice

--- src/shared/features/charactersSlice.ts
+++ src/shared/features/charactersSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import { Term } from '@services/localStorage/LSService';
 
-interface CharactersState {
+export interface CharactersState {
   selectedCharacters: string[];
 }
 
