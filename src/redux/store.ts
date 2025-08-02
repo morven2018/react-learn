086c176402fabdd8de20f4983fe04cb7ad326467
Alena <morven2018@yandex.ru>
2025-08-02 05:59:54 +0300

    feat: add redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
-import themeReducer from '../shared/features/themeSlice';
+import charactersSlice from '@shared/features/charactersSlice';
 import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 
 export const store = configureStore({
   reducer: {
-    theme: themeReducer,
+    characters: charactersSlice,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
