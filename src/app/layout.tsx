b3b0fd34905c211bff60de77b485721c5f297076
Alena <morven2018@yandex.ru>
2025-08-16 01:26:15 +0300

    fix: update path to icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import logo from '@assets/logo.svg';
+
 export default function RootLayout({
   children,
 }: {
@@ -7,8 +9,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="@assets/logo.svg" />
-        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="icon" type="image/svg+xml" href={logo} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Lord of the rings. Search</title>
         <meta name="description" content="Lord of the rings. Search on Next" />
