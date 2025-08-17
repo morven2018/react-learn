35c74217f3ec4b8e9e2310f3e82e01ce901a1559
Alena <morven2018@yandex.ru>
2025-08-17 05:09:40 +0300

    feat: apply style to 404

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import './[locale]/layout.scss';
 import type { ReactNode } from 'react';
+import { ThemeProvider } from '@/context/theme-provider';
 
 type Props = {
   children: ReactNode;
@@ -7,7 +9,9 @@ type Props = {
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
