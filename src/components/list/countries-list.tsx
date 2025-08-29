6c42075d96314793cf0c98da9c59d447c02f56d9
Alena <morven2018@yandex.ru>
2025-08-30 02:30:34 +0300

    feat: add preview to closed accordion

--- src/components/list/countries-list.tsx
+++ src/components/list/countries-list.tsx
@@ -1,10 +1,11 @@
 import CountryTable from '../table/table';
 import styles from './countries-list.module.scss';
-import { CO2Data, Country } from '../../shared/types/types';
+import { useRef } from 'react';
+import { CO2Data, Country, DataColumn } from '../../shared/types/types';
 
 interface CountriesListProps {
   countries: Country[];
-  onCountrySelect: (countryKey: string) => void;
+  onCountrySelect: (countryName: string) => void;
   selectedCountry: string | null;
   data: CO2Data;
 }
@@ -15,42 +16,116 @@ const CountriesList: React.FC<CountriesListProps> = ({
   selectedCountry,
   data,
 }) => {
+  const countryRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+
+  const handleCountrySelect = (countryName: string) => {
+    const isOpening = selectedCountry !== countryName;
+    onCountrySelect(isOpening ? countryName : '');
+
+    if (isOpening && countryRefs.current[countryName]) {
+      setTimeout(() => {
+        countryRefs.current[countryName]?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }, 100);
+    }
+  };
+
+  const handleHideTable = (countryName: string) => {
+    onCountrySelect('');
+    setTimeout(() => {
+      countryRefs.current[countryName]?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }, 100);
+  };
+
+  const setCountryRef =
+    (countryName: string) => (el: HTMLDivElement | null) => {
+      countryRefs.current[countryName] = el;
+    };
+
   return (
     <div className={styles.list}>
-      {countries.map((country) => (
-        <div key={country.name} className={styles.countryItem}>
-          <div
-            className={`${styles.accordionHeader} ${
-              selectedCountry === country.name ? styles.selected : ''
-            }`}
-            onClick={() =>
-              onCountrySelect(
-                selectedCountry === country.name ? '' : country.name
-              )
-            }
-          >
-            <div className={styles.countryInfo}>
-              <div className={styles.countryName}>{country.name}</div>
-              <div className={styles.countryDetails}>
-                {country.iso_code && `ISO: ${country.iso_code}`}
-              </div>
-              <div className={styles.countryDetails}>
-                {country.population &&
-                  `Population: ${country.population.toLocaleString()}`}
-              </div>
-            </div>
-            <span className={styles.accordionIcon}>
-              {selectedCountry === country.name ? '-' : '+'}
-            </span>
-          </div>
+      {countries.map((country) => {
+        const countryData = data[country.name]?.data || [];
+        const sortedData = [...countryData].sort((a, b) => b.year - a.year);
+        const previewData = sortedData.slice(0, 5);
+        const hasMoreData = countryData.length > 5;
+        const hasData = countryData.length > 0;
 
-          {selectedCountry === country.name && (
-            <div className={styles.accordionContent}>
-              <CountryTable data={data[country.name]?.data || []} />
+        return (
+          <div
+            key={country.name}
+            className={styles.countryItem}
+            ref={setCountryRef(country.name)}
+          >
+            <div
+              className={`${styles.accordionHeader} ${
+                selectedCountry === country.name ? styles.selected : ''
+              }`}
+              onClick={() => handleCountrySelect(country.name)}
+            >
+              <div className={styles.countryInfo}>
+                <div className={styles.countryName}>{country.name}</div>
+                <div className={styles.countryDetails}>
+                  {`ISO: ${country.iso_code}`}
+                </div>
+                <div className={styles.countryDetails}>
+                  {`Population: ${country.population}`}
+                </div>
+              </div>
+              <span className={styles.accordionIcon}>
+                {selectedCountry === country.name ? '-' : '+'}
+              </span>
             </div>
-          )}
-        </div>
-      ))}
+
+            {selectedCountry !== country.name && hasData && (
+              <div className={styles.previewContent}>
+                <CountryTable
+                  data={previewData}
+                  columns={[
+                    DataColumn.YEAR,
+                    DataColumn.POPULATION,
+                    DataColumn.CO2,
+                    DataColumn.CO2_PER_CAPITA,
+                  ]}
+                />
+
+                {hasMoreData && (
+                  <div className={styles.moreButton}>
+                    <button onClick={() => handleCountrySelect(country.name)}>
+                      More
+                    </button>
+                  </div>
+                )}
+              </div>
+            )}
+
+            {selectedCountry === country.name && hasData && (
+              <div className={styles.fullContent}>
+                <CountryTable
+                  data={sortedData}
+                  columns={[
+                    DataColumn.YEAR,
+                    DataColumn.POPULATION,
+                    DataColumn.CO2,
+                    DataColumn.CO2_PER_CAPITA,
+                  ]}
+                />
+
+                <div className={styles.hideButton}>
+                  <button onClick={() => handleHideTable(country.name)}>
+                    Hide
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
