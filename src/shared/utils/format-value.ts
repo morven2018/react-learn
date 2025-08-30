82779e04d0597226ae07505fb20c52bbb6edccf3
Alena <morven2018@yandex.ru>
2025-08-30 03:04:42 +0300

    fix: update formatter

--- src/shared/utils/format-value.ts
+++ src/shared/utils/format-value.ts
@@ -1,6 +1,12 @@
-const formatValue = (value: number | null | undefined): string => {
-  if (value === null || value === undefined) return 'N/A';
+const formatValue = (
+  value: number | null | undefined | string,
+  isYear: boolean = false
+): string => {
+  if (value === null || value === undefined || typeof value === 'string')
+    return 'N/A';
+  if (isYear) return value.toString();
   if (Number.isInteger(value)) return value.toLocaleString();
   return value.toFixed(2);
 };
+
 export default formatValue;
