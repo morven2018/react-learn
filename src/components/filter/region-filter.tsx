efe5e994cf108e3a49073b58f1301e1227f813af
Alena <morven2018@yandex.ru>
2025-08-31 05:50:28 +0300

    feat: optimize filter

--- src/components/filter/region-filter.tsx
+++ src/components/filter/region-filter.tsx
@@ -1,3 +1,4 @@
+import React, { useCallback, useMemo } from 'react';
 import styles from './region-filter.module.scss';
 import { getAllRegions } from '../../shared/utils/get-region';
 
@@ -6,33 +7,37 @@ interface RegionFilterProps {
   onRegionChange: (region: string) => void;
 }
 
-const RegionFilter: React.FC<RegionFilterProps> = ({
-  selectedRegion,
-  onRegionChange,
-}: RegionFilterProps) => {
-  const regions = ['All', ...getAllRegions()];
+const RegionFilter: React.FC<RegionFilterProps> = React.memo(
+  ({ selectedRegion, onRegionChange }: RegionFilterProps) => {
+    const regions = useMemo(() => ['All', ...getAllRegions()], []);
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    onRegionChange(event.target.value);
-  };
+    const handleChange = useCallback(
+      (event: React.ChangeEvent<HTMLSelectElement>) => {
+        onRegionChange(event.target.value);
+      },
+      [onRegionChange]
+    );
 
-  return (
-    <div className={styles.regionFilter}>
-      <label htmlFor="region-select">Filter by Region:</label>
-      <select
-        id="region-select"
-        value={selectedRegion}
-        onChange={handleChange}
-        className={styles.regionSelect}
-      >
-        {regions.map((region) => (
-          <option key={region} value={region}>
-            {region}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-};
+    return (
+      <div className={styles.regionFilter}>
+        <label htmlFor="region-select">Filter by Region:</label>
+        <select
+          id="region-select"
+          value={selectedRegion}
+          onChange={handleChange}
+          className={styles.regionSelect}
+        >
+          {regions.map((region) => (
+            <option key={region} value={region}>
+              {region}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+);
+
+RegionFilter.displayName = 'RegionFilter';
 
 export default RegionFilter;
