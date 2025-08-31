3c95c26521171cc7fa80b646213adaa77fff9c01
Alena <morven2018@yandex.ru>
2025-08-31 17:22:58 +0300

    fix: remove sonar issue

--- src/components/fallback/loading-fallback.tsx
+++ src/components/fallback/loading-fallback.tsx
@@ -3,11 +3,7 @@ import styles from './fallback.module.scss';
 const Loading: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-      <div
-        className={styles.spinner}
-        role="status"
-        aria-label="Loading..."
-      ></div>
+      <output className={styles.spinner} aria-label="Loading..."></output>
       <span className={styles.title}>Loading...</span>
     </div>
   );
